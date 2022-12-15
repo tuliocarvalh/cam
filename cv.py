42d9a315d7fe07df5f150dce7d496d8a7ab92f24
tuliocarvalh <tuliocarvalh@gmail.com>
2022-12-15 13:32:57 -0300

    First codes

---
+++ cv.py
@@ -0,0 +1,61 @@
+import cv2
+
+
+img1 = cv2.imread("/content/opencv/laranja.png")
+img2 = cv2.imread("/content/opencv/maca.png")
+ 
+# Juntando as duas imagens lado a lado
+final_image = cv2.hconcat((img1, img2))
+ 
+# Exibindo a imagem no colab
+#cv2.imshow(final_image)
+
+
+# Verificando o tipo de dados da matriz da imagem
+print('Dtype da imagem1 é {} e o Dtype da imagem2 é {}'
+     .format(img1.dtype,img2.dtype))
+# Verificando a altura da imagem
+print('A altura da imagem1 é {} e a altura da imagem2 é {}'
+     .format(img1.shape[0],img2.shape[0]))
+# Verificando a largura da imagem
+print('A largura da imagem1 é {} e a largura da imagem2 é {}'
+     .format(img1.shape[1],img2.shape[1]))
+# Verificando o número de canais da imagem
+print('O número de canais da imagem1 é {} e o número de canais da imagem2 é {}'
+    .format(img1.shape[2],img2.shape[2]))
+ 
+
+
+#b1, g1, r1 = cv.split(img1)
+#b2, g2, r2 = cv.split(img2)
+## Verificando a cor b da imagem
+#print('Elementos diferente de zero na imagem1 é de {} e na imagem2 é de {}'
+#     .format(cv.countNonZero(b1),cv.countNonZero(b2)))
+## Verificando a cor g da imagem
+#print('Elementos diferente de zero na imagem1 é de {} e na imagem2 é de {}'
+#     .format(cv.countNonZero(g1),cv.countNonZero(g2)))
+## Verificando a cor r da imagem
+#print('Elementos diferente de zero na imagem1 é de {} e na imagem2 é de {}'
+#     .format(cv.countNonZero(r1),cv.countNonZero(r2)))
+#
+#
+#def image_difference(image_1, image_2):
+#  # Salva o shape das imagens
+#  img1_shape = image_1.shape[:2]
+#  img2_shape = image_2.shape[:2]
+#  # TESTE 1: Compara a estrutura das imagens
+#  if img1_shape == img2_shape:
+#    print("O tamanho das imagens são os mesmos")
+#    # Extrai a diferença de cor entre duas imagens
+#    difference = cv.subtract(image_1, image_2)
+#    # Separa as três cores da imagem
+#    b, g, r = cv.split(difference)
+#    # TESTE 2: Compara as cores das imagens
+#    if cv.countNonZero(b) == 0 and cv.countNonZero(g) == 0 and cv.countNonZero(r) == 0:
+#      print("As cores das imagens são iguais")
+#    else:
+#      print('As cores das imagens são diferentes')
+#  else:
+#    print("As imagens tem tamanhos diferentes")
+#    # Gera uma imagem com a diferença das imagens
+#  return cv2_imshow(difference)
